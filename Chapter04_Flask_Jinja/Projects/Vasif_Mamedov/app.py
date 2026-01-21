a4c0d11d0e14fbdf45ad1f3beb6c259bd73b24e5
VasifMamedov <vasif.mamedov.1@iliauni.edu.ge>
2026-01-22 02:01:13 +0400

    updated chapter_4

--- Chapter04_Flask_Jinja/Projects/Vasif_Mamedov/app.py
+++ Chapter04_Flask_Jinja/Projects/Vasif_Mamedov/app.py
@@ -2,16 +2,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 products = [
-    {"name":"s25", "price":100, "img":"/static/s25.jpg" },
-    {"name":"s25", "price":100, "img":"/static/s25.jpg" },
-    {"name":"s25", "price":100, "img":"/static/s25.jpg" },
-    {"name":"s25", "price":100, "img":"/static/s25.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":0, "name":"s25", "price":100, "img":"/static/s25.jpg" },
+    {"id":1, "name":"s25", "price":100, "img":"/static/s25.jpg" },
+    {"id":2, "name":"s25", "price":100, "img":"/static/s25.jpg" },
+    {"id":3, "name":"s25", "price":100, "img":"/static/s25.jpg" },
+    {"id":4, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":5, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":6, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":7, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":8, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
+    {"id":9, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
 ]
 
 @app.route('/')
@@ -23,6 +23,14 @@ def register():
 @app.route('/login')
 def login():
     return render_template("login.html")
+
+@app.route('/view_product/<int:product_id>')
+def view_product(product_id):
+    return render_template("view_product.html", product=products[product_id] )
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
