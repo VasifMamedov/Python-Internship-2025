d7a3ec214c8fd5f72eea616a26794744d469c042
VasifMamedov <vasif.mamedov.1@iliauni.edu.ge>
2026-01-24 23:13:25 +0400

    updated chapter_4

--- Chapter05_Forms/Projects/Vasif_Mamedov/app.py
+++ Chapter05_Forms/Projects/Vasif_Mamedov/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
+from forms import RegisterForm
 app = Flask(__name__)
-
+app.config["SECRET_KEY"] = "AWDWQDASDSAFHSFJWIasjdalskfjqwoijf@1234098"
+products = []
 products = [
     {"id":0, "name":"s25", "price":100, "img":"/static/s25.jpg" },
     {"id":1, "name":"s25", "price":100, "img":"/static/s25.jpg" },
@@ -12,16 +14,24 @@ products = [
     {"id":7, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
     {"id":8, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
     {"id":9, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
-    {"id":10, "name":"nokia3310", "price":200, "img":"/static/Nokia3310.png" },
-{"id":11, "name":"iphone17", "price":200, "img":"/static/iphone17.jpg" },
 ]
+
+users=[]
 
 @app.route('/')
 def index():
-    return render_template("index.html", product_list=products)
-@app.route('/register')
+    return render_template("index.html", users=users, product_list=products)
+@app.route('/register', methods=["GET", "POST"])
 def register():
-    return render_template("register.html")
+    form = RegisterForm()
+    if form.validate_on_submit():
+            users.append(
+                {"username": form.username.data,
+                 "password": form.password.data,}
+            )
+    else:
+        print("Something went wrong")
+    return render_template("register.html", form=form)
 @app.route('/login')
 def login():
     return render_template("login.html")
